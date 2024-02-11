a0bf2fc7b15cad4806bcfdfce06d9123394c3ef9
soramicha <schang7jcu@gmail.com>
2024-02-11 15:51:26 -0800

    Finished Task

--- src/app/api/example/route.ts
+++ src/app/api/example/route.ts
@@ -1,5 +1,5 @@
 import connectDB from "@database/db";
-import { NextResponse } from "../../../../node_modules/next/server";
+import { NextResponse } from "next/server";
 //import { NextResponse } from "next/server";
 
 /**
--- src/app/components/success.tsx
+++ src/app/components/success.tsx
@@ -1,11 +1,12 @@
-import style from './success.module.css'
+import style from '/styles/success.module.css'
+import Link from "../../../node_modules/next/link";
 
 export default function SuccessComponent() {
     return (
     <div className={style.box}>
         <h1 className={style.title}>Success!</h1>
         <h3 className={style.message}>You have successfully signed up for an account.</h3>
-        <button className={style.login}>Back to Login</button>
+        <Link href="/pages/login"><button className={style.login}>Back to Login</button></Link>
     </div>
     );
   }

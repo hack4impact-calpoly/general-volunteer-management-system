fe8b861392ee3057276014c159ef133531832af3
whiniy <winnie.t835@gmail.com>
2024-03-07 17:41:01 -0800

    login changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
 import Navbar from "@components/Navbar";
+import Login from "../app/pages/authentication/login/page"
 
 export default function Home() {
   return (
     <main>
+      <Navbar></Navbar>
       <h1>Volunteer Management System Home Page</h1>
+      <Login></Login>
     </main>
   );
 }
